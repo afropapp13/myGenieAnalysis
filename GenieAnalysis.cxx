db041061654a94984be11dadccc3b0d1e62704e4
afropapp13 <afropapp@yahoo.gr>
2020-09-21 10:59:35 -0500

    v43 Sept 21 2020:  freezing Genie processing code, though still waiting on v3.0.4 sample

--- GenieAnalysis.cxx
+++ GenieAnalysis.cxx
@@ -19,28 +19,10 @@
 
 #include "../myClasses/Tools.h"
 #include "../myClasses/STV_Tools.h"
+#include "../../../../../Secondary_Code/myFunctions.cpp"
 
 using namespace std;
 using namespace Constants;
-
-void Reweight(TH1D* h, double SF) {
-
-	int NBins = h->GetXaxis()->GetNbins();
-
-	for (int i = 0; i < NBins; i++) {
-
-		double CurrentEntry = h->GetBinContent(i+1);
-		double NewEntry = CurrentEntry * SF / h->GetBinWidth(i);
-
-		double CurrentError = h->GetBinError(i+1);
-		double NewError = CurrentError * SF / h->GetBinWidth(i);
-
-		h->SetBinContent(i+1,NewEntry); 
-		h->SetBinError(i+1,NewError); 
-
-	}
-
-}
 
 void GenieAnalysis::Loop() {
 
@@ -56,151 +38,6 @@ void GenieAnalysis::Loop() {
 	std::cout << std::endl << std::endl;
 
 	// ---------------------------------------------------------------------------------------------------------------------------------	
-
-	// 1D Reco Transverse Variables
-
-	TH1D* RecoDeltaPTPlot = new TH1D("RecoDeltaPTPlot",LabelXAxisDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH1D* RecoDeltaAlphaTPlot = new TH1D("RecoDeltaAlphaTPlot",LabelXAxisDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH1D* RecoDeltaPhiTPlot = new TH1D("RecoDeltaPhiTPlot",LabelXAxisDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH1D* RecoMuonMomentumPlot = new TH1D("RecoMuonMomentumPlot",LabelXAxisMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH1D* RecoMuonPhiPlot = new TH1D("RecoMuonPhiPlot",LabelXAxisMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH1D* RecoMuonCosThetaPlot = new TH1D("RecoMuonCosThetaPlot",LabelXAxisMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH1D* RecoProtonMomentumPlot = new TH1D("RecoProtonMomentumPlot",LabelXAxisProtonMomentum,NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH1D* RecoProtonPhiPlot = new TH1D("RecoProtonPhiPlot",LabelXAxisProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH1D* RecoProtonCosThetaPlot = new TH1D("RecoProtonCosThetaPlot",LabelXAxisProtonCosTheta,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH1D* RecoECalPlot = new TH1D("RecoECalPlot",LabelXAxisECal,NBinsECal,ArrayNBinsECal);
-	TH1D* RecoEQEPlot = new TH1D("RecoEQEPlot",LabelXAxisEQE,NBinsEQE,ArrayNBinsEQE);
-	TH1D* RecoQ2Plot = new TH1D("RecoQ2Plot",LabelXAxisQ2,NBinsQ2,ArrayNBinsQ2);
-
-	TH1D* RecokMissPlot = new TH1D("RecokMissPlot",LabelXAxiskMiss,NBinskMiss,ArrayNBinskMiss);
-	TH1D* RecoPMissMinusPlot = new TH1D("RecoPMissMinusPlot",LabelXAxisPMissMinus,NBinsPMissMinus,ArrayNBinsPMissMinus);
-	TH1D* RecoPMissPlot = new TH1D("RecoPMissPlot",LabelXAxisPMiss,NBinsPMiss,ArrayNBinsPMiss);
-
-	// ---------------------------------------------------------------------------------------------------------------------------------	
-
-	// CC1p 1D Reco True Transverse Variables
-
-	TH1D* CC1pRecoDeltaPTPlot = new TH1D("CC1pRecoDeltaPTPlot",LabelXAxisDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH1D* CC1pRecoDeltaAlphaTPlot = new TH1D("CC1pRecoDeltaAlphaTPlot",LabelXAxisDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH1D* CC1pRecoDeltaPhiTPlot = new TH1D("CC1pRecoDeltaPhiTPlot",LabelXAxisDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH1D* CC1pRecoMuonMomentumPlot = new TH1D("CC1pRecoMuonMomentumPlot",LabelXAxisMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH1D* CC1pRecoMuonPhiPlot = new TH1D("CC1pRecoMuonPhiPlot",LabelXAxisMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH1D* CC1pRecoMuonCosThetaPlot = new TH1D("CC1pRecoMuonCosThetaPlot",LabelXAxisMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH1D* CC1pRecoProtonMomentumPlot = new TH1D("CC1pRecoProtonMomentumPlot",LabelXAxisProtonMomentum,NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH1D* CC1pRecoProtonPhiPlot = new TH1D("CC1pRecoProtonPhiPlot",LabelXAxisProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH1D* CC1pRecoProtonCosThetaPlot = new TH1D("CC1pRecoProtonCosThetaPlot",LabelXAxisProtonCosTheta,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH1D* CC1pRecoECalPlot = new TH1D("CC1pRecoECalPlot",LabelXAxisECal,NBinsECal,ArrayNBinsECal);
-	TH1D* CC1pRecoEQEPlot = new TH1D("CC1pRecoEQEPlot",LabelXAxisEQE,NBinsEQE,ArrayNBinsEQE);
-	TH1D* CC1pRecoQ2Plot = new TH1D("CC1pRecoQ2Plot",LabelXAxisQ2,NBinsQ2,ArrayNBinsQ2);
-
-	TH1D* CC1pTrueDeltaPTPlot = new TH1D("CC1pTrueDeltaPTPlot",LabelXAxisDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH1D* CC1pTrueDeltaAlphaTPlot = new TH1D("CC1pTrueDeltaAlphaTPlot",LabelXAxisDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH1D* CC1pTrueDeltaPhiTPlot = new TH1D("CC1pTrueDeltaPhiTPlot",LabelXAxisDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH1D* CC1pTrueMuonMomentumPlot = new TH1D("CC1pTrueMuonMomentumPlot",LabelXAxisMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH1D* CC1pTrueMuonPhiPlot = new TH1D("CC1pTrueMuonPhiPlot",LabelXAxisMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH1D* CC1pTrueMuonCosThetaPlot = new TH1D("CC1pTrueMuonCosThetaPlot",LabelXAxisMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH1D* CC1pTrueProtonMomentumPlot = new TH1D("CC1pTrueProtonMomentumPlot",LabelXAxisProtonMomentum,NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH1D* CC1pTrueProtonPhiPlot = new TH1D("CC1pTrueProtonPhiPlot",LabelXAxisProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH1D* CC1pTrueProtonCosThetaPlot = new TH1D("CC1pTrueProtonCosThetaPlot",LabelXAxisProtonCosTheta,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH1D* CC1pTrueECalPlot = new TH1D("CC1pTrueECalPlot",LabelXAxisECal,NBinsECal,ArrayNBinsECal);
-	TH1D* CC1pTrueEQEPlot = new TH1D("CC1pTrueEQEPlot",LabelXAxisEQE,NBinsEQE,ArrayNBinsEQE);
-	TH1D* CC1pTrueQ2Plot = new TH1D("CC1pTrueQ2Plot",LabelXAxisQ2,NBinsQ2,ArrayNBinsQ2);
-
-	TH1D* CC1pRecokMissPlot = new TH1D("CC1pRecokMissPlot",LabelXAxiskMiss,NBinskMiss,ArrayNBinskMiss);
-	TH1D* CC1pRecoPMissMinusPlot = new TH1D("CC1pRecoPMissMinusPlot",LabelXAxisPMissMinus,NBinsPMissMinus,ArrayNBinsPMissMinus);
-	TH1D* CC1pRecoPMissPlot = new TH1D("CC1pRecoPMissPlot",LabelXAxisPMiss,NBinsPMiss,ArrayNBinsPMiss);
-
-	// 2D Reco True Transverse Variables
-
-	TH2D* CC1pRecoTrueDeltaPTPlot2D = new TH2D("CC1pRecoTrueDeltaPTPlot2D",
-		LabelXAxisDeltaPT2D,NBinsDeltaPT,ArrayNBinsDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH2D* CC1pRecoTrueDeltaAlphaTPlot2D = new TH2D("CC1pRecoTrueDeltaAlphaTPlot2D",
-		LabelXAxisDeltaAlphaT2D,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH2D* CC1pRecoTrueDeltaPhiTPlot2D = new TH2D("CC1pRecoTrueDeltaPhiTPlot2D",
-		LabelXAxisDeltaPhiT2D,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH2D* CC1pRecoTrueMuonMomentumPlot2D = new TH2D("CC1pRecoTrueMuonMomentumPlot2D",LabelXAxisMuonMomentum2D,NBinsMuonMomentum,
-		ArrayNBinsMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH2D* CC1pRecoTrueMuonPhiPlot2D = new TH2D("CC1pRecoTrueMuonPhiPlot2D",LabelXAxisMuonPhi2D,NBinsMuonPhi,
-		ArrayNBinsMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH2D* CC1pRecoTrueMuonCosThetaPlot2D = new TH2D("CC1pRecoTrueMuonCosThetaPlot2D",LabelXAxisMuonCosTheta2D,NBinsMuonCosTheta,
-		ArrayNBinsMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH2D* CC1pRecoTrueProtonMomentumPlot2D = new TH2D("CC1pRecoTrueProtonMomentumPlot2D",
-		LabelXAxisProtonMomentum2D,NBinsProtonMomentum,ArrayNBinsProtonMomentum,NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH2D* CC1pRecoTrueProtonPhiPlot2D = new TH2D("CC1pRecoTrueProtonPhiPlot2D",LabelXAxisProtonPhi2D,NBinsProtonPhi,
-		ArrayNBinsProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH2D* CC1pRecoTrueProtonCosThetaPlot2D = new TH2D("CC1pRecoTrueProtonCosThetaPlot2D",
-		LabelXAxisProtonCosTheta2D,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH2D* CC1pRecoTrueECalPlot2D = new TH2D("CC1pRecoTrueECalPlot2D",LabelXAxisECal2D,NBinsECal,ArrayNBinsECal,NBinsECal,ArrayNBinsECal);
-	TH2D* CC1pRecoTrueEQEPlot2D = new TH2D("CC1pRecoTrueEQEPlot2D",LabelXAxisEQE2D,NBinsEQE,ArrayNBinsEQE,NBinsEQE,ArrayNBinsEQE);
-	TH2D* CC1pRecoTrueQ2Plot2D = new TH2D("CC1pRecoTrueQ2Plot2D",LabelXAxisQ22D,NBinsQ2,ArrayNBinsQ2,NBinsQ2,ArrayNBinsQ2);
-
-	// --------------------------------------------------------------------------------------------------------------------------------------------
-
-	// Bkg 1D Reco True Transverse Variables
-
-	TH1D* BkgRecoTrueDeltaPTPlot = new TH1D("NonCC1pRecoDeltaPTPlot",LabelXAxisDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH1D* BkgRecoTrueDeltaAlphaTPlot = new TH1D("NonCC1pRecoDeltaAlphaTPlot",LabelXAxisDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH1D* BkgRecoTrueDeltaPhiTPlot = new TH1D("NonCC1pRecoDeltaPhiTPlot",LabelXAxisDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH1D* BkgRecoTrueMuonMomentumPlot = new TH1D("NonCC1pRecoMuonMomentumPlot",LabelXAxisMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH1D* BkgRecoTrueMuonPhiPlot = new TH1D("NonCC1pRecoMuonPhiPlot",LabelXAxisMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH1D* BkgRecoTrueMuonCosThetaPlot = new TH1D("NonCC1pRecoMuonCosThetaPlot",LabelXAxisMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH1D* BkgRecoTrueProtonMomentumPlot = new TH1D("NonCC1pRecoProtonMomentumPlot",LabelXAxisProtonMomentum,
-		NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH1D* BkgRecoTrueProtonPhiPlot = new TH1D("NonCC1pRecoProtonPhiPlot",LabelXAxisProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH1D* BkgRecoTrueProtonCosThetaPlot = new TH1D("NonCC1pRecoProtonCosThetaPlot",LabelXAxisProtonCosTheta,
-		NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH1D* BkgRecoTrueECalPlot = new TH1D("NonCC1pRecoECalPlot",LabelXAxisECal,NBinsECal,ArrayNBinsECal);
-	TH1D* BkgRecoTrueEQEPlot = new TH1D("NonCC1pRecoEQEPlot",LabelXAxisEQE,NBinsEQE,ArrayNBinsEQE);
-	TH1D* BkgRecoTrueQ2Plot = new TH1D("NonCC1pRecoQ2Plot",LabelXAxisQ2,NBinsQ2,ArrayNBinsQ2);
-
-	TH1D* NonCC1pRecokMissPlot = new TH1D("NonCC1pRecokMissPlot",LabelXAxiskMiss,NBinskMiss,ArrayNBinskMiss);
-	TH1D* NonCC1pRecoPMissMinusPlot = new TH1D("NonCC1pRecoPMissMinusPlot",LabelXAxisPMissMinus,NBinsPMissMinus,ArrayNBinsPMissMinus);
-	TH1D* NonCC1pRecoPMissPlot = new TH1D("NonCC1pRecoPMissPlot",LabelXAxisPMiss,NBinsPMiss,ArrayNBinsPMiss);
-
-	// 2D Reco True Transverse Variables
-
-	TH2D* BkgRecoTrueDeltaPTPlot2D = new TH2D("NonCC1pRecoDeltaPTPlot2D",
-		LabelXAxisDeltaPT2D,NBinsDeltaPT,ArrayNBinsDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
-	TH2D* BkgRecoTrueDeltaAlphaTPlot2D = new TH2D("NonCC1pRecoDeltaAlphaTPlot2D",
-		LabelXAxisDeltaAlphaT2D,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
-	TH2D* BkgRecoTrueDeltaPhiTPlot2D = new TH2D("NonCC1pRecoDeltaPhiTPlot2D",
-		LabelXAxisDeltaPhiT2D,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT,NBinsDeltaPhiT,ArrayNBinsDeltaPhiT);
-
-	TH2D* BkgRecoMuonMomentumPlot2D = new TH2D("BkgRecoMuonMomentumPlot2D",LabelXAxisMuonMomentum2D,NBinsMuonMomentum,
-		ArrayNBinsMuonMomentum,NBinsMuonMomentum,ArrayNBinsMuonMomentum);
-	TH2D* BkgRecoMuonPhiPlot2D = new TH2D("BkgRecoMuonPhiPlot2D",LabelXAxisMuonPhi2D,NBinsMuonPhi,ArrayNBinsMuonPhi,NBinsMuonPhi,ArrayNBinsMuonPhi);
-	TH2D* BkgRecoMuonCosThetaPlot2D = new TH2D("BkgRecoMuonCosThetaPlot2D",LabelXAxisMuonCosTheta2D,NBinsMuonCosTheta,
-		ArrayNBinsMuonCosTheta,NBinsMuonCosTheta,ArrayNBinsMuonCosTheta);
-
-	TH2D* BkgRecoProtonMomentumPlot2D = new TH2D("BkgRecoProtonMomentumPlot2D",
-		LabelXAxisProtonMomentum2D,NBinsProtonMomentum,ArrayNBinsProtonMomentum,NBinsProtonMomentum,ArrayNBinsProtonMomentum);
-	TH2D* BkgRecoProtonPhiPlot2D = new TH2D("BkgRecoProtonPhiPlot2D",LabelXAxisProtonPhi2D,
-		NBinsProtonPhi,ArrayNBinsProtonPhi,NBinsProtonPhi,ArrayNBinsProtonPhi);
-	TH2D* BkgRecoProtonCosThetaPlot2D = new TH2D("BkgRecoProtonCosThetaPlot2D",
-		LabelXAxisProtonCosTheta2D,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta,NBinsProtonCosTheta,ArrayNBinsProtonCosTheta);
-
-	TH2D* BkgRecoTrueECalPlot2D = new TH2D("NonCC1pRecoECalPlot2D",LabelXAxisECal2D,NBinsECal,ArrayNBinsECal,NBinsECal,ArrayNBinsECal);
-	TH2D* BkgRecoTrueEQEPlot2D = new TH2D("NonCC1pRecoEQEPlot2D",LabelXAxisEQE2D,NBinsEQE,ArrayNBinsEQE,NBinsEQE,ArrayNBinsEQE);
-	TH2D* BkgRecoTrueQ2Plot2D = new TH2D("NonCC1pRecoQ2Plot2D",LabelXAxisQ22D,NBinsQ2,ArrayNBinsQ2,NBinsQ2,ArrayNBinsQ2);
-
-	// -----------------------------------------------------------------------------------------------------------------------------------------------
-
-	// 1D True Transverse Variables
 
 	TH1D* TrueDeltaPTPlot = new TH1D("TrueDeltaPTPlot",LabelXAxisDeltaPT,NBinsDeltaPT,ArrayNBinsDeltaPT);
 	TH1D* TrueDeltaAlphaTPlot = new TH1D("TrueDeltaAlphaTPlot",LabelXAxisDeltaAlphaT,NBinsDeltaAlphaT,ArrayNBinsDeltaAlphaT);
@@ -221,6 +58,17 @@ void GenieAnalysis::Loop() {
 	TH1D* TruekMissPlot = new TH1D("TruekMissPlot",LabelXAxiskMiss,NBinskMiss,ArrayNBinskMiss);
 	TH1D* TruePMissMinusPlot = new TH1D("TruePMissMinusPlot",LabelXAxisPMissMinus,NBinsPMissMinus,ArrayNBinsPMissMinus);
 	TH1D* TruePMissPlot = new TH1D("TruePMissPlot",LabelXAxisPMiss,NBinsPMiss,ArrayNBinsPMiss);
+
+	// For now and until box opening
+	int NBins2DAnalysis = 4;
+		
+	TH2D* TrueCosThetaMuPmuPlot = new TH2D("TrueCosThetaMuPmuPlot",LabelXAxisMuonCosTheta+LabelXAxisMuonMomentum
+			,NBins2DAnalysis,ArrayNBinsMuonCosTheta[0],ArrayNBinsMuonCosTheta[NBinsMuonCosTheta]
+			,NBins2DAnalysis,ArrayNBinsMuonMomentum[0],ArrayNBinsMuonMomentum[NBinsMuonMomentum]);
+			
+	TH2D* TrueCosThetaPPpPlot = new TH2D("TrueCosThetaPPpPlot",LabelXAxisProtonCosTheta+LabelXAxisProtonMomentum
+			,NBins2DAnalysis,ArrayNBinsProtonCosTheta[0],ArrayNBinsProtonCosTheta[NBinsProtonCosTheta]
+			,NBins2DAnalysis,ArrayNBinsProtonMomentum[0],ArrayNBinsProtonMomentum[NBinsProtonMomentum]);	
 
 	// ------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -430,6 +278,11 @@ void GenieAnalysis::Loop() {
 				TruePMissMinusPlot->Fill(TruePMissMinus,weight);
 				TruePMissPlot->Fill(TrueMissMomentum,weight);
 
+				// 2D Analysis
+		
+				TrueCosThetaMuPmuPlot->Fill(MuonCosTheta,MuonMomentum,weight);
+				TrueCosThetaPPpPlot->Fill(ProtonCosTheta,ProtonMomentum,weight);	
+
 			}
 
 		} // End of the selection cuts && the demand that we fill the plots with the same events
@@ -475,6 +328,7 @@ void GenieAnalysis::Loop() {
 
 	if (OutFileName == "Genie_v3_0_6_uB_Tune_1" || OutFileName == "Genie_v3_0_6_Out_Of_The_Box") { ScalingFactor = ScalingFactor * G18_10a_02_11a_FluxIntegratedXSection ; }
 	if (OutFileName == "SuSav2") { ScalingFactor = ScalingFactor * SuSav2FluxIntegratedXSection ; }
+	if (OutFileName == "GENIEv2") { ScalingFactor = ScalingFactor * R_2_12_10_FluxIntegratedXSection ; }
 
 	file->cd();
 
@@ -502,6 +356,9 @@ void GenieAnalysis::Loop() {
 	Reweight(TruekMissPlot,ScalingFactor);
 	Reweight(TruePMissPlot,ScalingFactor);
 	Reweight(TruePMissMinusPlot,ScalingFactor);
+
+	Reweight2D(TrueCosThetaMuPmuPlot,ScalingFactor);
+	Reweight2D(TrueCosThetaPPpPlot,ScalingFactor);
 	
 	// --------------------------------------------------------------------------------------------------------------------------------------------	
 	
